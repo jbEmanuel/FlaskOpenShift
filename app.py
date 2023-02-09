eb5c300a57059a968baacbac91b84273fa067dc9
João Beza Emanuel <87081984+jbEmanuel@users.noreply.github.com>
2023-02-09 13:22:31 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask 
+from os import environ
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    env_var = environ.get('FLASK_SECRET')
+    message = f"Hello Flask!, this is my openshift secret {env_var}"
+    return message 
+
+if __name__=="__main__":
+    app.debug = True 
+    app.run()
