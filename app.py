f412a44ed18573870156e5fd79648f4937f8c5ad
João Beza Emanuel <87081984+jbEmanuel@users.noreply.github.com>
2023-02-10 08:58:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     env_var = environ.get('FLASK_SECRET')
-    message = f"Hello Flask!, this is my openshift secret {env_var}"
+    message = f"Hello Flask!, this is my openshift secret: {env_var}"
     return message 
 
 if __name__=="__main__":
